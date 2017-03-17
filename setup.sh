11d9e52e3c0b3606adc7ec1d991efc5ef17f41c3
John Pinkerton <jpinkerton88@gmail.com>
2017-03-17 13:06:58 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,6 +3,9 @@ sudo -v
 # Keep-alive: update existing `sudo` time stamp until script has finished
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
+# install xcode command line tools
+xcode-select --install
+
 echo "symlinking main files"
 ln -sf ~/.dotfiles/aliases ~/.aliases
 ln -sf ~/.dotfiles/gitconfig ~/.gitconfig
