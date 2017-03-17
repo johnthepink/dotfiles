394059065cefcaea90ee4e496b91944ca3c5206d
John Pinkerton <jpinkerton88@gmail.com>
2017-03-17 13:02:21 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -37,6 +37,9 @@ tic assets/xterm-256color-italic.terminfo
 echo "installing homebrew"
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
+echo "installling gpg"
+brew install gpg gpg-agent pinentry-mac
+
 echo "installing node"
 brew install node
 
@@ -45,12 +48,11 @@ brew install yarn
 
 echo "installing ruby"
 brew install ruby
+gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+curl -sSL https://get.rvm.io | bash -s stable
 
 echo "installing hub"
 brew install hub
-
-echo "installling gpg"
-brew install gpg gpg-agent pinentry-mac
 
 echo "installing tmux"
 brew install tmux
