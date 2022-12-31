8566b0b497778ce8dfb1722a2c13f8874ecba430
John Pinkerton <jpinkerton88@gmail.com>
2022-12-31 01:23:44 -0500

    adjust italics

--- nvim/lua/config/plugins/tokyo.lua
+++ nvim/lua/config/plugins/tokyo.lua
@@ -13,15 +13,22 @@ function tokyo.config()
       colors.gitSigns.delete = colors.red
       colors.gitSigns.change = colors.orange
     end,
+    styles = {
+      -- Style to be applied to different syntax groups
+      -- Value is any valid attr-list value for `:help nvim_set_hl`
+      comments = { italic = true },
+      keywords = { italic = true },
+      functions = { italic = true },
+      variables = {},
+    },
   })
 
   vim.cmd([[
-    highlight Comment cterm=italic gui=italic
     colorscheme tokyonight
 
     augroup highlight_yank
-    autocmd!
-    au TextYankPost * silent! lua vim.highlight.on_yank({higroup="IncSearch", timeout=200})
+      autocmd!
+      au TextYankPost * silent! lua vim.highlight.on_yank({higroup="IncSearch", timeout=200})
     augroup END
   ]])
 end
