25a3906bf880c6ef1d6143a5c3fd00cad73c986f
John Pinkerton <jpinkerton88@gmail.com>
2023-01-25 17:33:16 -0500

    disable lualine on tree instead of workaround

--- nvim/lua/config/plugins/lualine.lua
+++ nvim/lua/config/plugins/lualine.lua
@@ -4,21 +4,15 @@ local M = {
 }
 
 function M.config()
-  local nvim_tree_shift = {
-    function()
-      local win = require("nvim-tree.view").get_winnr() or 0
-      return string.rep(" ", vim.api.nvim_win_get_width(win) - 1)
-    end,
-    cond = require("nvim-tree.view").is_visible,
-    color = "NvimTreeNormal",
-  }
-
   require("lualine").setup({
     options = {
       theme = "tokyonight",
+      disabled_filetypes = {
+        "NvimTree",
+      },
     },
     sections = {
-      lualine_a = { nvim_tree_shift, "mode" }, -- dont show when tree open
+      lualine_a = { "mode" }, -- dont show when tree open
       lualine_x = { "filetype" }, -- remove encoding and format
     },
   })
