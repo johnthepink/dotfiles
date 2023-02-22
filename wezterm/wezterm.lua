8b3336c0e9d66bfc3edb7aef7f7bd80a3966fe18
John Pinkerton <jpinkerton88@gmail.com>
2023-02-21 20:50:56 -0500

    formatting

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,24 +1,19 @@
 local wezterm = require("wezterm")
 
 return {
-	term = "wezterm",
-
-	-- color_scheme = "tokyonight",
-
-	font_size = 16,
-	font = wezterm.font("JetBrainsMono Nerd Font"),
-	color_scheme_dirs = { "~/.config/wezterm" },
-	color_scheme = "tokyonight_storm",
-
-	use_fancy_tab_bar = true,
-	tab_bar_at_bottom = true,
-	hide_tab_bar_if_only_one_tab = true,
-	show_tab_index_in_tab_bar = false,
-
-	window_padding = {
-		left = 0,
-		right = 0,
-		top = 0,
-		bottom = 0,
-	},
+  term = "wezterm",
+  font_size = 16,
+  font = wezterm.font("JetBrainsMono Nerd Font"),
+  color_scheme_dirs = { "~/.config/wezterm" },
+  color_scheme = "tokyonight_storm",
+  use_fancy_tab_bar = true,
+  tab_bar_at_bottom = true,
+  hide_tab_bar_if_only_one_tab = true,
+  show_tab_index_in_tab_bar = false,
+  window_padding = {
+    left = 0,
+    right = 0,
+    top = 0,
+    bottom = 0,
+  },
 }
