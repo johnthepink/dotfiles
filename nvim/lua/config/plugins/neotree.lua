871aa602252ae7832364c80e37262c98074b884e
John Pinkerton <jpinkerton88@gmail.com>
2023-03-07 11:05:51 -0500

    convert to string

--- nvim/lua/config/plugins/neotree.lua
+++ nvim/lua/config/plugins/neotree.lua
@@ -59,7 +59,7 @@ return {
   init = function()
     vim.g.neo_tree_remove_legacy_commands = 1
     if vim.fn.argc() == 1 then
-      local stat = vim.loop.fs_stat(vim.fn.argv(0))
+      local stat = vim.loop.fs_stat(tostring(vim.fn.argv(0)))
       if stat and stat.type == "directory" then
         require("neo-tree.command").execute({ toggle = true, reveal = true })
       end
