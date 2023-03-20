211c797f3eaffab51e3e0aabd78e496be0bdfff4
John Pinkerton <jpinkerton88@gmail.com>
2023-03-20 12:37:09 -0400

    remove rvm

--- setup.sh
+++ setup.sh
@@ -41,11 +41,6 @@ brew install node
 echo "installing yarn"
 brew install yarn
 
-echo "installing ruby"
-brew install ruby
-gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
-curl -sSL https://get.rvm.io | bash -s stable
-
 echo "installing git"
 brew install git
 
