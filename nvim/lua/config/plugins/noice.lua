3a2981ecd91e7b96e8b39c8c5e0d2b075ad3194d
John Pinkerton <jpinkerton88@gmail.com>
2023-08-23 09:09:58 -0400

    filter no info messages from multiple LPSs

--- nvim/lua/config/plugins/noice.lua
+++ nvim/lua/config/plugins/noice.lua
@@ -21,11 +21,11 @@ return {
       },
       -- you can enable a preset for easier configuration
       presets = {
-        bottom_search = true, -- use a classic bottom cmdline for search
-        command_palette = true, -- position the cmdline and popupmenu together
+        bottom_search = true,         -- use a classic bottom cmdline for search
+        command_palette = true,       -- position the cmdline and popupmenu together
         long_message_to_split = true, -- long messages will be sent to a split
-        inc_rename = false, -- enables an input dialog for inc-rename.nvim
-        lsp_doc_border = false, -- add a border to hover docs and signature help
+        inc_rename = false,           -- enables an input dialog for inc-rename.nvim
+        lsp_doc_border = false,       -- add a border to hover docs and signature help
       },
       routes = {
         -- don't show written messages in popup
@@ -34,6 +34,14 @@ return {
             event = "msg_show",
             kind = "",
             find = "written",
+          },
+          opts = { skip = true },
+        },
+        -- don't show noise from LSPs
+        {
+          filter = {
+            event = "notify",
+            find = "No information available",
           },
           opts = { skip = true },
         },
