2175d70f8fbe2fa8d1b641577cc8c12535efcc23
John Pinkerton <jpinkerton88@gmail.com>
2023-09-14 16:58:47 -0400

    fix ts comment string

--- nvim/lua/config/plugins/mini.lua
+++ nvim/lua/config/plugins/mini.lua
@@ -11,9 +11,9 @@ end
 
 function mini.comment()
   require("mini.comment").setup({
-    hooks = {
-      pre = function()
-        require("ts_context_commentstring.internal").update_commentstring({})
+    options = {
+      custom_commentstring = function()
+        return require("ts_context_commentstring.internal").calculate_commentstring() or vim.bo.commentstring
       end,
     },
   })
