0003b706b64255dd88156187f5a4e7ce8674ce7f
John Pinkerton <jpinkerton88@gmail.com>
2023-11-07 16:18:35 -0500

    add rails tests shortcuts

--- nvim/lua/config/mappings.lua
+++ nvim/lua/config/mappings.lua
@@ -12,3 +12,16 @@ map("n", "<leader>tl", ":set linebreak!<cr>")
 map("n", "<leader>ts", ":set spell!<cr>")
 map("n", "<C-t>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
 map("n", "<C-g>", "<cmd>silent !tmux neww repome<CR>")
+-- run all rails tests in new tmux pane
+map("n", "<leader>qq", "<cmd>silent !tmux neww zsh -c 'bin/rails test; read'<cr>")
+-- run current buffer rails tests
+map("n", "<leader>qf", function()
+  local path = vim.fn.expand("%:p")
+  return "<cmd>silent !tmux neww zsh -c 'bin/rails test " .. path .. "; read'<cr>"
+end, { expr = true })
+-- run current buffer rails test at cursor position
+map("n", "<leader>ql", function()
+  local path = vim.fn.expand("%:p")
+  local position = vim.fn.getcurpos()[2]
+  return "<cmd>silent !tmux neww zsh -c 'bin/rails test " .. path .. ":" .. position .. "; read'<cr>"
+end, { expr = true })
