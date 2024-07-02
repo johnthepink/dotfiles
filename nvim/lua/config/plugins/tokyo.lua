744814a6cae4843ab84129fb6284b8ee448e45b8
John Pinkerton <jpinkerton88@gmail.com>
2024-07-02 15:32:14 -0400

    updates

--- nvim/lua/config/plugins/tokyo.lua
+++ nvim/lua/config/plugins/tokyo.lua
@@ -2,14 +2,13 @@ local tokyo = {
   "folke/tokyonight.nvim",
   lazy = false,
   priority = 1000,
-  commit = "096543e112744eb94947cc3c5a916fff1deea2f0",
   opts = {
     style = "storm",
     lualine_bold = true,
-    on_colors = function(colors)
-      colors.gitSigns.add = colors.green
-      colors.gitSigns.delete = colors.red
-      colors.gitSigns.change = colors.orange
+    on_highlights = function(highlights, colors)
+      highlights.GitSignsAdd.fg = colors.green
+      highlights.GitSignsDelete.fg = colors.red
+      highlights.GitSignsChange.fg = colors.orange
     end,
   },
 }
