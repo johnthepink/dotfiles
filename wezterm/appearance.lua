fb60eb234f210939d074c558b4f4965d06bc9767
John Pinkerton <jpinkerton88@gmail.com>
2024-08-16 09:13:57 -0400

    playing with wezterm

---
+++ wezterm/appearance.lua
@@ -0,0 +1,11 @@
+local wezterm = require 'wezterm'
+local module = {}
+
+function module.is_dark()
+  if wezterm.gui then
+    return wezterm.gui.get_appearance():find("Dark")
+  end
+  return true
+end
+
+return module
--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,19 +1,21 @@
-local wezterm = require("wezterm")
+local wezterm = require("wezterm");
+local config = wezterm.config_builder();
+local appearance = require("appearance");
 
-return {
-  term = "wezterm",
+if appearance.is_dark() then
+  config.color_scheme = 'Tokyo Night Storm'
+else
+  config.color_scheme = 'Tokyo Night Day'
+end
+
+config.font_size = 16;
+config.font = wezterm.font("JetBrainsMono Nerd Font");
+config.tab_bar_at_bottom = true;
+config.show_tab_index_in_tab_bar = false;
+
+config.window_frame = {
+  font = wezterm.font({ family = 'JetBrainsMono Nerd Font', weight = 'Bold' }),
   font_size = 16,
-  font = wezterm.font("JetBrainsMono Nerd Font"),
-  color_scheme_dirs = { "~/.config/wezterm" },
-  color_scheme = "tokyonight_storm",
-  use_fancy_tab_bar = true,
-  tab_bar_at_bottom = true,
-  hide_tab_bar_if_only_one_tab = true,
-  show_tab_index_in_tab_bar = false,
-  window_padding = {
-    left = 0,
-    right = 0,
-    top = 0,
-    bottom = 0,
-  },
 }
+
+return config;
