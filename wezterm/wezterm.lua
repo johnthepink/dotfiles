28fb4db8dcc76cdcde8a8cd20f7ffde487815ed6
John Pinkerton <jpinkerton88@gmail.com>
2025-08-09 21:25:06 -0400

    replace custom tabline with tabline plugin

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -2,6 +2,7 @@ local wezterm = require('wezterm')
 local config = wezterm.config_builder()
 local appearance = require('appearance')
 local projects = require('projects')
+local tabline = wezterm.plugin.require('https://github.com/michaelbrusegard/tabline.wez')
 
 local tokyo = wezterm.color.get_builtin_schemes()['Tokyo Night Storm']
 tokyo.ansi[3] = '#73daca'
@@ -9,6 +10,56 @@ tokyo.brights[3] = '#73daca'
 config.color_schemes = {
   ['Tokyo Night Storm Mod'] = tokyo,
 }
+
+tabline.setup({
+  options = {
+    icons_enabled = true,
+    theme = tokyo,
+    theme_overrides = {
+      tab = {
+        active = { bg = tokyo.brights[1] },
+      },
+    },
+    section_separators = {
+      left = wezterm.nerdfonts.pl_left_hard_divider,
+      right = wezterm.nerdfonts.pl_right_hard_divider,
+    },
+    component_separators = {
+      left = wezterm.nerdfonts.pl_left_soft_divider,
+      right = wezterm.nerdfonts.pl_right_soft_divider,
+    },
+    tab_separators = {
+      left = wezterm.nerdfonts.pl_left_hard_divider,
+      right = wezterm.nerdfonts.pl_right_hard_divider,
+    },
+  },
+  sections = {
+    tabline_a = { 'mode' },
+    tabline_b = {
+      'workspace',
+    },
+    tabline_c = {},
+    tab_active = {
+      { 'process', padding = { left = 0, right = 0 }, icons_only = true },
+      { 'zoomed', padding = 0 },
+    },
+    tab_inactive = {
+      { 'process', padding = { left = 0, right = 0 }, icons_only = true },
+    },
+    tabline_x = {},
+    tabline_y = { 'ram', 'cpu', 'battery' },
+    tabline_z = { { 'datetime', style = '%I:%M' } },
+  },
+  extensions = {},
+})
+
+-- specific tabline config
+config.use_fancy_tab_bar = false
+config.tab_bar_at_bottom = false
+config.hide_tab_bar_if_only_one_tab = false
+config.window_decorations = 'NONE'
+
+tabline.apply_to_config(config)
 
 local function set_color_scheme()
   if appearance.is_dark() then
@@ -26,8 +77,6 @@ end)
 
 config.font_size = 16
 config.font = wezterm.font('JetBrainsMono Nerd Font')
-config.tab_bar_at_bottom = true
-config.show_tab_index_in_tab_bar = false
 config.native_macos_fullscreen_mode = true
 config.scrollback_lines = 10000
 
@@ -130,58 +179,5 @@ config.keys = {
     action = wezterm.action.ToggleFullScreen,
   },
 }
-
-local function segments_for_right_status(window)
-  return {
-    -- %Y-%m-%d  %I:%M
-    -- wezterm.strftime('%a %b %-d %H:%M'),
-    wezterm.strftime('%I:%M'),
-    wezterm.strftime('%Y-%m-%d'),
-    window:active_workspace(),
-  }
-end
-
-wezterm.on('update-status', function(window, _)
-  local SOLID_LEFT_ARROW = wezterm.nerdfonts.pl_right_hard_divider
-  local right_segments = segments_for_right_status(window)
-
-  local color_scheme = window:effective_config().resolved_palette
-  local bg = wezterm.color.parse(color_scheme.background)
-  local fg = color_scheme.foreground
-
-  local gradient_to, gradient_from = bg, bg
-  if appearance.is_dark() then
-    gradient_from = gradient_to:lighten(0.2)
-  else
-    gradient_from = gradient_to:darken(0.2)
-  end
-
-  local right_gradient = wezterm.color.gradient(
-    {
-      orientation = 'Horizontal',
-      colors = { gradient_from, gradient_to },
-    },
-    #right_segments -- only gives us as many colours as we have segments.
-  )
-
-  -- We'll build up the elements to send to wezterm.format in this table.
-  local right_elements = {}
-
-  for i, seg in ipairs(right_segments) do
-    local is_first = i == 1
-
-    if is_first then
-      table.insert(right_elements, { Background = { Color = 'none' } })
-    end
-    table.insert(right_elements, { Foreground = { Color = right_gradient[i] } })
-    table.insert(right_elements, { Text = SOLID_LEFT_ARROW })
-
-    table.insert(right_elements, { Foreground = { Color = fg } })
-    table.insert(right_elements, { Background = { Color = right_gradient[i] } })
-    table.insert(right_elements, { Text = ' ' .. seg .. ' ' })
-  end
-
-  window:set_right_status(wezterm.format(right_elements))
-end)
 
 return config
