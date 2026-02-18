7be007c44aa1e9cf5e8763f2cc41054b638760e3
John Pinkerton <jpinkerton88@gmail.com>
2026-02-17 20:25:56 -0500

    add git hunk shortcuts

--- nvim/lua/config/plugins/init.lua
+++ nvim/lua/config/plugins/init.lua
@@ -42,7 +42,29 @@ return {
       local gitsigns = require('gitsigns')
       gitsigns.setup({
         on_attach = function(buffer)
-          vim.keymap.set('n', '<leader>tb', gitsigns.blame, { buffer = buffer })
+          local function map(mode, l, r, opts)
+            opts = opts or {}
+            opts.buffer = buffer
+            vim.keymap.set(mode, l, r, opts)
+          end
+
+          map('n', ']c', function()
+            if vim.wo.diff then
+              vim.cmd.normal({ ']c', bang = true })
+            else
+              gitsigns.nav_hunk('next')
+            end
+          end)
+
+          map('n', '[c', function()
+            if vim.wo.diff then
+              vim.cmd.normal({ '[c', bang = true })
+            else
+              gitsigns.nav_hunk('prev')
+            end
+          end)
+
+          map('n', '<leader>tb', gitsigns.blame)
         end,
       })
     end,
